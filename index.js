bb5881a95c999bb7e0e85d7251e39f9e5b0bb2eb
Saurav Sreejith <techgenius2005@gmail.com>
2024-08-18 00:37:48 +0530

    submission

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require("multer")
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post("/api/fileanalyse", upload.single("upfile"), async (req,res) => {
+
+  let file  = req.file
+
+  if (!file) return res.send("No file")
+
+  return res.send({ name: file.originalname, type: file.mimetype, size: file.size})
+})
 
 
 
